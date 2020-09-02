a26f6da6f56eb3f25de757aae88dc264e8ba7535
Julien Delferiere <julien.d@tymate.com>
2020-09-02 13:03:12 +0200

    Initial Commit

---
+++ beergram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  beergram
+//
+//  Created by Julien Delferiere on 02/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ beergram/beergramApp.swift
@@ -0,0 +1,17 @@
+//
+//  beergramApp.swift
+//  beergram
+//
+//  Created by Julien Delferiere on 02/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct beergramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
